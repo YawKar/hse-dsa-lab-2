d800e6dd82fecd9cb0138d523c761cdb39af3659
yawkar <yawkarpov@gmail.com>
2023-03-22 16:03:40 +0300

    refactor: little refactoring of benchmarks

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,9 @@
 const int XSEED = 42;
 const int YSEED = 13;
 
-static void BM_NaiveRectangleEnumeration(benchmark::State& state) {
+// NaiveRectangleEnumeration
+static void BM_Building_NaiveRectangleEnumeration(benchmark::State& state) {
   for (auto _ : state) {
-    
     state.PauseTiming();
     auto rectangles = TestCaseGenerator::generateRecommendedRectangles(state.range(0));
     auto points = TestCaseGenerator::generateUniformlyDistributedPoints(state.range(0), 0, state.range(0) * 20, 0, state.range(0) * 20, XSEED, YSEED);
@@ -17,17 +17,34 @@ static void BM_NaiveRectangleEnumeration(benchmark::State& state) {
     state.ResumeTiming();
     
     algorithm.buildInternals();
-    for (const auto& point : points) {
-      algorithm.queryPoint(point);
-    }
   }
 }
 
-BENCHMARK(BM_NaiveRectangleEnumeration)->ArgName("Rectangles&Points")->DenseRange(10, 100, 10)->Iterations(10);
-BENCHMARK(BM_NaiveRectangleEnumeration)->ArgName("Rectangles&Points")->DenseRange(100, 1000, 100)->Iterations(10);
-BENCHMARK(BM_NaiveRectangleEnumeration)->ArgName("Rectangles&Points")->DenseRange(10000, 100000, 10000)->Iterations(3);
+BENCHMARK(BM_Building_NaiveRectangleEnumeration)->ArgName("Rectangles&Points")->DenseRange(10, 100, 10)->Iterations(100000);
+BENCHMARK(BM_Building_NaiveRectangleEnumeration)->ArgName("Rectangles&Points")->DenseRange(200, 1000, 100)->Iterations(100000);
+BENCHMARK(BM_Building_NaiveRectangleEnumeration)->ArgName("Rectangles&Points")->DenseRange(10000, 100000, 10000)->Iterations(10000);
 
-static void BM_QubicMapBuilding(benchmark::State& state) {
+static void BM_PerRequest_NaiveRectangleEnumeration(benchmark::State& state) {
+  auto rectangles = TestCaseGenerator::generateRecommendedRectangles(state.range(0));
+  auto points = TestCaseGenerator::generateUniformlyDistributedPoints(state.range(0), 0, state.range(0) * 20, 0, state.range(0) * 20, XSEED, YSEED);
+  int currentIdx = 0;
+  auto algorithm = NaiveRectangleEnumeration(std::move(rectangles));
+  algorithm.buildInternals();
+
+  for (auto _ : state) {
+    algorithm.queryPoint(points[currentIdx++]);
+    state.PauseTiming();
+    currentIdx %= points.size();
+    state.ResumeTiming();
+  }
+}
+
+BENCHMARK(BM_PerRequest_NaiveRectangleEnumeration)->ArgName("Rectangles&Points")->DenseRange(10, 100, 10)->Iterations(100000);
+BENCHMARK(BM_PerRequest_NaiveRectangleEnumeration)->ArgName("Rectangles&Points")->DenseRange(200, 1000, 100)->Iterations(100000);
+BENCHMARK(BM_PerRequest_NaiveRectangleEnumeration)->ArgName("Rectangles&Points")->DenseRange(10000, 100000, 10000)->Iterations(10000);
+
+// QubicMapBuilding
+static void BM_Building_QubicMapBuilding(benchmark::State& state) {
   for (auto _ : state) {
     state.PauseTiming();
     auto rectangles = TestCaseGenerator::generateRecommendedRectangles(state.range(0));
@@ -36,14 +53,30 @@ static void BM_QubicMapBuilding(benchmark::State& state) {
     state.ResumeTiming();
     
     algorithm.buildInternals();
-    for (const auto& point : points) {
-      algorithm.queryPoint(point);
-    }
   }
 }
 
-BENCHMARK(BM_QubicMapBuilding)->ArgName("Rectangles&Points")->DenseRange(10, 100, 10)->Iterations(10);
-BENCHMARK(BM_QubicMapBuilding)->ArgName("Rectangles&Points")->DenseRange(100, 1000, 100)->Iterations(10);
-BENCHMARK(BM_QubicMapBuilding)->ArgName("Rectangles&Points")->DenseRange(1000, 5000, 1000)->Iterations(2);
+BENCHMARK(BM_Building_QubicMapBuilding)->ArgName("Rectangles&Points")->DenseRange(10, 100, 10);
+BENCHMARK(BM_Building_QubicMapBuilding)->ArgName("Rectangles&Points")->DenseRange(200, 1000, 100);
+BENCHMARK(BM_Building_QubicMapBuilding)->ArgName("Rectangles&Points")->DenseRange(2000, 5000, 1000);
+
+static void BM_PerRequest_QubicMapBuilding(benchmark::State& state) {
+  auto rectangles = TestCaseGenerator::generateRecommendedRectangles(state.range(0));
+  auto points = TestCaseGenerator::generateUniformlyDistributedPoints(state.range(0), 0, state.range(0) * 20, 0, state.range(0) * 20, XSEED, YSEED);
+  int currentIdx = 0;
+  auto algorithm = QubicMapBuilding(std::move(rectangles));
+  algorithm.buildInternals();
+
+  for (auto _ : state) {
+    algorithm.queryPoint(points[currentIdx++]);
+    state.PauseTiming();
+    currentIdx %= points.size();
+    state.ResumeTiming();
+  }
+}
+
+BENCHMARK(BM_PerRequest_QubicMapBuilding)->ArgName("Rectangles&Points")->DenseRange(10, 100, 10);
+BENCHMARK(BM_PerRequest_QubicMapBuilding)->ArgName("Rectangles&Points")->DenseRange(200, 1000, 100);
+BENCHMARK(BM_PerRequest_QubicMapBuilding)->ArgName("Rectangles&Points")->DenseRange(2000, 5000, 1000);
 
 BENCHMARK_MAIN();
